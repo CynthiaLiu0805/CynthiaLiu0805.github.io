4dcdd6efc5b48778450b85b560c3caf094fd21ea
Cynthia Liu <liuy363@mcmaster.ca>
2021-12-17 23:26:31 -0500

    add pictures

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function show() {
- 
-    /* Access image by id and change
-    the display property to block*/
-    document.getElementById('faceU')
-            .style.display = "block";
-
-    // document.getElementById('btnID')
-    //         .style.display = "none";
-}
+$(document).ready(function() {
+    $('.image-link').magnificPopup({type:'image'});
+  });
